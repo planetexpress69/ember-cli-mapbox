2ea51746640e89e204e58111c7b4037f9fc48af2
Martin <planetexpress69@users.noreply.github.com>
2015-12-21 10:18:38 +0100

    Fixed property typo.

--- addon/components/mapbox-marker.js
+++ addon/components/mapbox-marker.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   classNameBindings: ['isLoaded'],
   layout: layout,
   iconUrl: '',
-  iconUrl: '',
+  iconRetinaUrl: '',
   iconSize: [30, 45],
   iconAnchor: [15, 45],
   popupAnchor: [0, -45],
